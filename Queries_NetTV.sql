f40666b8548454bafdd54ad6c0dac1ea77de3d55
Taku1103 <orzorz1243@gmail.com>
2023-11-12 17:16:19 +0900

    README修正,ファイルをsql化

--- Queries_NetTV.sql
+++ Queries_NetTV.sql
@@ -169,28 +169,3 @@ INNER JOIN (
         ジャンル名) AS MaxView
 ON ga.ジャンル名 = MaxView.ジャンル名 AND ga.エピソード平均視聴回数 = MaxView.最大視聴回数
 ;
-
-
-
-
-
-(not正解)
-SELECT
-gen.ジャンル名,
-pro.番組名,
-ROUND(AVG(epi.エピソード再生数)) AS エピソード平均視聴回数
-FROM programs AS pro
-INNER JOIN episodes AS epi
-ON epi.番組ID = pro.番組ID
-INNER JOIN programs_genres AS pg
-ON pg.番組ID = pro.番組ID
-INNER JOIN genres AS gen
-ON gen.ジャンルID = pg.ジャンルID
-GROUP BY
-gen.ジャンル名,
-pro.番組名
-;
-
-
-
-
